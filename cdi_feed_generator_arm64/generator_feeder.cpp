94c931be0401b77de870d602d87b9c891bf9e08e
Acherontas <145214445+Acherontas@users.noreply.github.com>
2025-11-13 14:09:16 +0200

    Add files via upload

---
+++ cdi_feed_generator_arm64/generator_feeder.cpp
@@ -0,0 +1,47 @@
+#include "generator_feeder.h"
+#include <iomanip>
+#include "math.h"
+#include "sum_cdi.h"
+using namespace std;
+//ctor
+generator_feeder::generator_feeder(){}
+//dtor
+generator_feeder::~generator_feeder(){}
+//copy ctor
+generator_feeder::generator_feeder(const generator_feeder& other){}
+//handle self assignment//assignment operator
+generator_feeder& generator_feeder::operator=(const generator_feeder& rhs){if (this == &rhs) return *this; return *this;}
+int generator_feeder::gener_feed(){
+        sum_cdi *scd=new sum_cdi();
+        long double xi;
+        xend=0;
+        cout<<"\n";
+        cout<<"for cdi of " << f_cdi <<" with row out " << row_out <<"\n";
+    for(int i=0;i<10;i++){
+        xi=0;
+         if(lt_of_each[i]>1){
+         nn=pow(10,(lt_of_each[i]-1));
+         startt=nn*ch_le[i];
+         endd=startt+(nn-1);
+         }
+         if(lt_of_each[i]==1){
+         nn=pow(10,1);
+         startt=1;
+         endd=9;
+         }
+
+         cout<<"\n";
+         cout<<"using " << startt << " " << endd <<"\n";
+        for(int i=startt;i<=endd;i++){
+            xi=scd->cdis(i,3);
+            if(xi==f_cdi){
+                   cout<< i <<" ";
+                   if(xend%row_out==0){cout<<"\n";}
+                   xend+=1;
+            }
+        }
+    }
+    cout<<" row out " << row_out <<"\n";
+    return 0;
+}
+
